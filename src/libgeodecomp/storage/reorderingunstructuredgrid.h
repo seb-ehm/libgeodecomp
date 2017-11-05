5dba3ab113b2c75e6dc642ed88dca2e8463bfc83
Sebastian Ehmann <sebastian@dipolmoment.de>
2017-11-05 23:08:22 +0100

    corrected typo that led to endless loop

--- src/libgeodecomp/storage/reorderingunstructuredgrid.h
+++ src/libgeodecomp/storage/reorderingunstructuredgrid.h
@@ -249,8 +249,7 @@ public:
 		for (RowLengthVec::iterator i = reorderedRowLengths.begin(); i != reorderedRowLengths.end(); ) {
 			std::size_t currentIndex = i - reorderedRowLengths.begin();
 			std::size_t nextIndex = (std::min)(currentIndex + SIGMA, reorderedRowLengths.size());
-			RowLengthVec::iterator nextStop = reorderedRowLengths.begin() + currentIndex;
-
+			RowLengthVec::iterator nextStop = reorderedRowLengths.begin() + nextIndex;
 			std::stable_sort(i, nextStop, [](const IntPair& a, const IntPair& b) {
 				return a.second > b.second;
 			});
